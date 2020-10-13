5b88d624fdc77d9ef567a94ab22bbeed1d244732
Gaurav Sethi <gauravsethi0404@gmail.com>
2020-10-13 00:26:02 -0400

    fixed file reader and added file writer

--- FileReader.py
+++ FileReader.py
@@ -1,13 +1,23 @@
-import sys
 class FileReader:
     def __init__(self):
+        self.readFile()
+
+    def readFile(self):
         f = open("demofile.txt", "r")
-        sstart = str.split(f.readline())
-        sgoal = str.split(f.readline())
+        sstart = list(map(int, str.split(f.readline())))
+        sgoal = list(map(int, str.split(f.readline())))
         hardTraverse = []
         for x in range(8):
-            hardTraverse.append(str.split(f.readline()))
+            hardTraverse.append(list(map(int, str.split(f.readline()))))
         completeGrid = []
         for x in range(120):
-            completeGrid.append(list(f.readline()))
+            row = list(f.readline())
+            row = row[:-1]
+            completeGrid.append(row)
         f.close()
+        print(sstart.__str__() + " " + sgoal.__str__() + " " + hardTraverse.__str__() + " " + completeGrid.__str__())
+        return sstart, sgoal, hardTraverse, completeGrid
+
+if __name__ == "__main__":
+    FileReader()
+
